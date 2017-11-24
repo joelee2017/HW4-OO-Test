53fefd12c801c34ebe6ecc579d6eaab271a1e7da
joelee2017 <as955133@gmail.com>
2017-11-24 11:47:08 +0800

    加入專案檔案。

---
+++ ClsTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace OOTest_CSharp
+{
+    public class A
+    {
+        public virtual void M1()
+        {
+            Debug.WriteLine("A.M1");
+        }
+
+        public virtual void M2()
+        {
+            Debug.WriteLine("A.M2");
+        }
+
+      
+    }
+
+
+    public class B : A
+    {
+
+        public override void M1()
+        {
+            Debug.WriteLine("B.M1");
+        }
+
+
+        public new void M2()
+        {
+            Debug.WriteLine("B.M2");
+        }
+
+
+
+    }
+
+    class C : B
+    {
+
+        public override void M1()
+        {
+            Debug.WriteLine("C.M1");
+        }
+
+
+        public new void M2(int i)
+        {
+            Debug.WriteLine("C.M2(i)");
+        }
+
+    }
+
+   
+}
---
+++ FrmTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace OOTest_CSharp
+{
+    public partial class FrmTest : Form
+    {
+        public FrmTest()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            A a = new A();
+
+            a.M1();
+            a.M2();
+
+
+            Debug.WriteLine("===================================");
+            B b = new B();
+            b.M1();
+            b.M2();
+          
+
+            Debug.WriteLine("===================================");
+            A x = b;
+            x.M1();
+            x.M2();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine("===================================");
+            C c = new C();
+            c.M1();
+
+            c.M2();  //遮蔽
+            c.M2(100);
+          
+
+
+            Debug.WriteLine("===================================");
+            A x = c;
+            x.M1();
+            x.M2();
+
+        }
+
+      
+    }
+}
